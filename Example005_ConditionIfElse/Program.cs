0a80ef10986a62159d4b4f33c786e65ecb2d9422
Your Name <dmitriirod@yandex.ru>
2023-02-21 21:55:41 +0300

    создали проект 5

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это жк МАША!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
