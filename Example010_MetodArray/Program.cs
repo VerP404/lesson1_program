8ada52521e5d37e1095e45ba76d8cc23b905ab53
Your Name <dmitriirod@yandex.ru>
2023-02-26 23:37:59 +0300

    Added eexample010

---
+++ Example010_MetodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = {1, 2, 5, 8 ,8 ,12, 5};
+
+int n = array.Length;
+int find = 5;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index]==find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
